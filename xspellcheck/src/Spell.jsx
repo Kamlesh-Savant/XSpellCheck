9915f31bbee14e95aa19a93f06b8a111c35559a6
Kamlesh Savant <sawantk45@gmail.com>
2024-07-29 13:56:14 +0530

    corrected code

--- xspellcheck/src/Spell.jsx
+++ xspellcheck/src/Spell.jsx
@@ -2,43 +2,49 @@ import React, { useState } from 'react';
 import './Spell.css';
 
 function Spell() {
+  const [correctSpell, setCorrectSpell] = useState('');
+  const [spell, setSpell] = useState('');
 
-    const [correctSpell, setCorrectSpell] = useState('');
-    const [spell, setSpell] = useState('');
+  const customDictionary = {
+    teh: 'the',
+    wrok: 'work',
+    fot: 'for',
+    exampl: 'example'
+  };
 
-    const customDictionary = {
-        teh: "the",
-        wrok: "work",
-        fot: "for",
-        exampl: "example"
-      };
-      
-    const handleChange = (e)=>{
+  const handleChange = (e) => {
+    const inputSpell = e.target.value;
+    setSpell(inputSpell);
 
-        setSpell(e.target.value);
+    const spelling = inputSpell.split(' ');
 
-        const spelling = spell.split(' ');
+    const ans = spelling.map((spells) => {
+      const correct_ans = customDictionary[spells.toLowerCase()];
+      return correct_ans || spells;
+    });
 
-        const ans = spelling.map((spells) =>{
-            const correct_ans = customDictionary[spells.toLowerCase()];
-            return correct_ans || spells;
-        });
+    const first_correction = ans.find((word, index) => word !== spelling[index]);
 
-        // const corrected_ans = ans.join(' ');
-
-        const first_correction = ans.find((word,index) => word !== spelling[index]);
-
-        setCorrectSpell(first_correction || "");
-
-    }
+    setCorrectSpell(first_correction || '');
+  };
 
   return (
     <div className='container'>
-        <h1>Spell Check and Auto-Correction</h1>
-        <textarea placeholder='Enter text...' rows={5} cols={60} onChange={handleChange} value={spell}></textarea>
-        {correctSpell&& <p>Did you mean: <strong> {correctSpell}</strong>?</p>}
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea
+        placeholder='Enter text...'
+        rows={5}
+        cols={60}
+        onChange={handleChange}
+        value={spell}
+      ></textarea>
+      {correctSpell && (
+        <p>
+          Did you mean: <strong>{correctSpell}</strong>?
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Spell
+export default Spell;
